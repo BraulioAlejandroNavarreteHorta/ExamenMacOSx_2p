d986c13b90446fa89ecbd9075f2c18113f3a8ed2
Moises Hernandez Alvarado <MoisesHernandezAlvarado@hotmail.com>
2023-05-18 17:06:12 -0600

    Añadi las nuevas clases de compras

---
+++ ExamenMacOSx_2p/ComprasController.swift
@@ -0,0 +1,8 @@
+//
+//  ComprasController.swift
+//  ExamenMacOSx_2p
+//
+//  Created by Moises Hernandez Alvarado on 18/05/23.
+//
+
+import Foundation
---
+++ ExamenMacOSx_2p/ComprasView.swift
@@ -0,0 +1,17 @@
+//
+//  ComprasView.swift
+//  ExamenMacOSx_2p
+//
+//  Created by Moises Hernandez Alvarado on 18/05/23.
+//
+
+import Cocoa
+
+class ComprasView: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
