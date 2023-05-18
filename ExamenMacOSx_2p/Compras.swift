0b525f89d4c0a57508cae6a11543727220ed54de
Moises Hernandez Alvarado <MoisesHernandezAlvarado@hotmail.com>
2023-05-18 17:19:37 -0600

    cree la clase de compra

--- ExamenMacOSx_2p/Compras.swift
+++ ExamenMacOSx_2p/Compras.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+class Compra: NSObject{
+
+    @objc dynamic var id:Int
+    @objc dynamic var idProducto:Int
+    @objc dynamic var cantidad:Int
+    @objc dynamic var idComprador:Int
+    
+    init(_ id: Int, _ idProducto: Int, _ cantidad: Int, _ idComprador: Int) {
+        self.id = id
+        self.idProducto = idProducto
+        self.cantidad = cantidad
+        self.idComprador = idComprador
+    }
+    
+}
